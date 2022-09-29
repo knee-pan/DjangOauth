d004306a9879e4b2b37b264e155d8ef15acbc12b
knee-pan <aysekazan95@gmail.com>
2022-09-29 16:27:23 +0300

    swagger created (localhos/doc)

--- config/urls.py
+++ config/urls.py
@@ -14,21 +14,51 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
-from django.conf.urls import url
-from rest_framework_swagger.views import get_swagger_view
+# from rest_framework_swagger.views import get_swagger_view
 
-schema_view = get_swagger_view(title="Swagger UI for  API")
+# If you haven't installed 'drf_yasg', swagger will not work.
+# schema_view1 = get_swagger_view(title="Swagger UI for  API")
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Jaseci API",
+        default_version="v1",
+        description="Welcome to the world of Jaseci",
+        terms_of_service="https://www.jaseci.org",
+        contact=openapi.Contact(email="jason@jaseci.org"),
+        license=openapi.License(name="Awesome IP"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("o/", include("oauth2_provider.urls", namespace="oauth2_provider1")),
     path("", include("account.api.urls", namespace="acc_api")),
     path("machine/", include("machine.api.urls")),
-    url(r"^$", schema_view),
+    # url("swg", schema_view1),
+    re_path(
+        r"^doc(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),  # <-- Here
+    path(
+        "doc/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),  # <-- Here
+    path(
+        "redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+    ),  # <-- Here
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 #  http://127.0.0.1:7000/o/applications/register/

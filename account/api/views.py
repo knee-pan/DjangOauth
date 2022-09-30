9332625d0fe8e8499aaadb5e864b41933503e79c
knee-pan <aysekazan95@gmail.com>
2022-09-30 13:52:41 +0300

    account api modeified

--- account/api/views.py
+++ account/api/views.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from models import User
 from oauth2_provider.views.generic import ProtectedResourceView
+from rest_framework.generics import CreateAPIView
+from serializer import UserSerializer
+from throttles import RegisterThrottle
 
 
 # 403
@@ -12,3 +16,9 @@ class ApiEndpoint(ProtectedResourceView):
 @login_required()
 def dashboard(request, *args, **kwargs):
     return HttpResponse("Login required", status=200)
+
+
+class UserCreate(CreateAPIView):
+    serializer_class = UserSerializer
+    throttle_classes = RegisterThrottle
+    queryset = User.objects.all()

29a8cc378350d837492c69a92417432e702a674d
knee-pan <aysekazan95@gmail.com>
2022-10-04 13:45:56 +0300

    models registered to admin.py

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from shop.models import Category, Product
+
+# admin.site.register(Category)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("title", "created")
+    list_filter = ("is_active",)
+    search_fields = ("title",)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "category")
+    list_filter = ("is_active",)
+    search_fields = ("title",)

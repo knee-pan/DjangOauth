939b5a36cbc81a7e4dbd946c951eb94c176a3c98
knee-pan <aysekazan95@gmail.com>
2022-10-05 09:29:50 +0300

    machine model created

--- machine/admin.py
+++ machine/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from machine.models import MachineType, Profile, Projector
+#from reversion.admin import VersionAdmin
+from machine.models import Machine, MachineType, Profile, Projector
 
 # Register your models here.
 
@@ -12,3 +13,28 @@ admin.site.register(Projector)
 @admin.register(MachineType)
 class MachineTypeAdmin(admin.ModelAdmin):
     list_display = ["__str__", "general_width", "general_height"]
+
+@admin.register(Machine)
+class MachineAdmin(admin.ModelAdmin): #class MachineAdmin(VersionAdmin):
+    list_display = [
+        "serial",
+        "mac",
+        "machine_type",
+        "projector_model",
+        "teamviewer_id",
+        "is_active",
+        "owner",
+        "last_activity",
+        "ip",
+    ]
+    list_filter = [
+        "is_active",
+        "projector_model",
+        "machine_type",
+    ]
+    search_fields = [
+        "serial",
+        "mac",]
+
+    autocomplete_fields = ["owner"]
+    ordering = ["serial"]
--- machine/models.py
+++ machine/models.py
@@ -1,3 +1,4 @@
+from datetime import timezone
 from django.core.exceptions import ValidationError
 from django.core.validators import (
     MaxValueValidator,
@@ -6,7 +7,7 @@ from django.core.validators import (
     RegexValidator,
 )
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
@@ -304,3 +305,88 @@ class Projector(models.Model):
 
     def __str__(self):
         return self.model
+
+
+class Machine(models.Model):
+    class RESIN_TYPE(models.TextChoices):
+        OPEN = "open", "Open Resin"
+        CLOSED = "close", "Closed Resin"
+
+    class SLICER_AUTOCENTER(models.IntegerChoices):
+        CENTER_PIECES = 0, "Center Pieces"
+        CENTER_ORIGIN = 1, "Center Origin"
+
+    is_active = models.BooleanField(default=True, verbose_name="Active", help_text="Machine status")
+
+    serial = models.CharField(max_length=15,unique=True,validators=[MinLengthValidator(12),RegexValidator(r"^[\d]*$", message="Only digit"),],)
+
+    owner = models.ForeignKey(User,on_delete=models.SET_NULL,null=True,related_name="machines",limit_choices_to={"applications__name": "MegaPlatform"},)
+    machine_type = models.ForeignKey(MachineType,on_delete=models.SET_NULL,null=True,blank=True,related_name="machines",)
+    mac = models.CharField(max_length=12,unique=True,validators=[MinLengthValidator(12), RegexValidator(r"^[A-F0-9]*$")],)
+
+    teamviewer_id = models.CharField(max_length=12,null=True,blank=True,validators=[MinLengthValidator(10),RegexValidator(r"^[\d]*$", message="Only digit"),],)
+    teamviewer_version = models.CharField(max_length=10, null=True, blank=True)
+
+    profiles = models.ManyToManyField(Profile, related_name="machines", limit_choices_to={"confirmed": True})
+
+    unconfirmed_profiles = models.ManyToManyField(Profile, blank=True, limit_choices_to={"confirmed": False})
+
+
+    check_status = models.BooleanField(default=False,verbose_name="Check Status",help_text="! Machine will check status on every actions. Connection required.",)
+    resin_type = models.CharField(max_length=5, choices=RESIN_TYPE.choices, default=RESIN_TYPE.OPEN)
+    enable_stl_slc = models.BooleanField(default=False,verbose_name="STL Support",help_text="Machine can slice stl files.",)
+
+    software_version = models.CharField(max_length=10, null=True, blank=True, verbose_name="Version (software)")
+    ip = models.GenericIPAddressField(verbose_name="Ip Address",protocol="both",unpack_ipv4=True,blank=True,null=True,)
+
+    real_plate_x = models.FloatField(default=120.0, help_text="(mm)")
+    print_area_x = models.IntegerField(default=1920, verbose_name="Print Area(X)")
+    print_area_y = models.IntegerField(default=1080, verbose_name="Print Area(Y)")
+    z_distance = models.FloatField(default=114, help_text="Z Distance (mm)")
+    led_current = models.PositiveSmallIntegerField(verbose_name="Projector Led Power",default=750,validators=[MinValueValidator(51), MaxValueValidator(1023)],help_text="51 - 1023",)
+    projector_model = models.ForeignKey(Projector, on_delete=models.SET_NULL, null=True, blank=True)
+    projector_serial = models.CharField(max_length=21,blank=True,validators=[MinLengthValidator(19),RegexValidator(r"^[a-zA-Z0-9]*$", "Only alphanumeric are allowed."),],)
+
+    heating_time = models.PositiveSmallIntegerField(default=30,validators=[MinValueValidator(5), MaxValueValidator(60)],help_text="Heating (min)",)
+    heater_close_time = models.PositiveSmallIntegerField(default=240, help_text="Heater Close (min)")
+    bed_heater_temp_limit = models.PositiveIntegerField(default=65,validators=[MinValueValidator(40), MaxValueValidator(90)],help_text="Max temp limit for heater. (40-90)",)
+
+    note = models.TextField(max_length=500, null=True, blank=True)
+
+    client_sync = models.DateTimeField(null=True, blank=True)
+    last_activity = models.DateTimeField(null=True, blank=True)
+
+    autocenter = models.IntegerField(choices=SLICER_AUTOCENTER.choices, default=SLICER_AUTOCENTER.CENTER_PIECES)
+
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        permissions = [
+            ("can_full_change", "Can Change Machine Full Permission"),
+        ]
+
+    def __str__(self):
+        return self.serial
+
+    @property
+    def active(self):
+        return self.is_active and self.owner.active
+
+    @property
+    def _has_mask(self):
+        return True if self.mask else False
+
+    def update_sync(self):
+        self.client_sync = timezone.now()
+        self.save()
+
+    def update_last_activity(self):
+        self.last_activity = timezone.now()
+        self.save()
+
+    def update_software_version(self, version):
+        if len(version) > 0:
+            if self.software_version != version:
+                self.software_version = version
+                self.save()

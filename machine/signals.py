77563da058fddd514010ac729584aa14d55df55c
knee-pan <aysekazan95@gmail.com>
2022-10-07 10:40:35 +0300

    bug fix

--- machine/signals.py
+++ machine/signals.py
@@ -2,14 +2,14 @@ from django.core.cache import cache
 from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 
-from .models import Machine
+from .models import Machine, PrintLog
 
 
 @receiver(post_delete, sender=Machine)
 def if_removed_clear_cache(sender, instance, **kwargs):
     # cache.clear()
-    cache.delete("log_count_cache")
-    # print(cache.delete("log_count_cache")) # Returns True if keys cached before
+    cache.delete("machine_count_cache")
+    # print(cache.delete("machine_count_cache")) # Returns True if keys cached before
 
 
 @receiver(post_save, sender=Machine)
@@ -19,3 +19,17 @@ def if_crated_clear_cache(sender, instance, created, **kwargs):
         print("calisti")
         # cache.clear() # sadece makine ile ilgili olanları değil her keyi siler.
         cache.delete("machine_count_cache")
+        # cache.delete_many(['a', 'b', 'c'])
+
+
+@receiver(post_delete, sender=PrintLog)
+def if_log_removed_clear_cache(sender, instance, **kwargs):
+    cache.delete("log_count_cache")
+
+
+@receiver(post_save, sender=PrintLog)
+def if_log_crated_clear_cache(sender, instance, created, **kwargs):
+    """Cache temizleniyor sessiondan atiyor"""
+    if created:
+        print("calisti")
+        cache.delete("log_count_cache")

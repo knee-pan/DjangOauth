08058aac20493087dcc00a286b442b8463b2583f
knee-pan <aysekazan95@gmail.com>
2022-10-07 10:28:06 +0300

    bug fix

--- machine/signals.py
+++ machine/signals.py
@@ -7,8 +7,9 @@ from .models import Machine
 
 @receiver(post_delete, sender=Machine)
 def if_removed_clear_cache(sender, instance, **kwargs):
-    cache.clear()
-    # cache.delete("objects")
+    # cache.clear()
+    cache.delete("log_count_cache")
+    # print(cache.delete("log_count_cache")) # Returns True if keys cached before
 
 
 @receiver(post_save, sender=Machine)
@@ -16,6 +17,5 @@ def if_crated_clear_cache(sender, instance, created, **kwargs):
     """Cache temizleniyor sessiondan atiyor"""
     if created:
         print("calisti")
-        cache.clear()
-
-    # cache.delete("objects")
+        # cache.clear() # sadece makine ile ilgili olanları değil her keyi siler.
+        print(cache.delete("machine_count_cache"))

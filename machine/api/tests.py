86b5aa48a96b33d766cff1e6ab3257f354e00b25
knee-pan <aysekazan95@gmail.com>
2022-10-07 15:03:36 +0300

    machine api/tests.py created

---
+++ machine/api/tests.py
@@ -0,0 +1,10 @@
+from rest_framework import status
+from rest_framework.test import APITestCase
+from machine.models import Machine, PrintLog
+from django.urls import reverse
+
+class TestMachine(APITestCase):
+    pass
+
+class TestPrintLog(APITestCase):
+    pass

f4a9c7061daedd05108670a704d9b545e8c0de5e
knee-pan <aysekazan95@gmail.com>
2022-10-10 12:00:04 +0300

    machine model signal modified

--- machine/signals.py
+++ machine/signals.py
@@ -8,28 +8,25 @@ from .models import Machine, PrintLog
 @receiver(post_delete, sender=Machine)
 def if_removed_clear_cache(sender, instance, **kwargs):
     # cache.clear()
-    cache.delete("machine_count_cache")
+    cache.delete("machine_machine_count_cache")
     # print(cache.delete("machine_count_cache")) # Returns True if keys cached before
 
 
 @receiver(post_save, sender=Machine)
 def if_crated_clear_cache(sender, instance, created, **kwargs):
-    """Cache temizleniyor sessiondan atiyor"""
     if created:
-        print("calisti")
-        # cache.clear() # sadece makine ile ilgili olanları değil her keyi siler.
-        cache.delete("machine_count_cache")
+        # cache.clear() # sadece makine ile ilgili olanları değil her keyi siler. Cache temizleniyor sessiondan atiyor
+        cache.delete("machine_machine_count_cache")
         # cache.delete_many(['a', 'b', 'c'])
 
 
 @receiver(post_delete, sender=PrintLog)
 def if_log_removed_clear_cache(sender, instance, **kwargs):
-    cache.delete("log_count_cache")
+    cache.delete("machine_printlog_count_cache")
 
 
 @receiver(post_save, sender=PrintLog)
 def if_log_crated_clear_cache(sender, instance, created, **kwargs):
-    """Cache temizleniyor sessiondan atiyor"""
     if created:
-        print("calisti")
-        cache.delete("log_count_cache")
+        print("machine_printlog_count_cache")
+        cache.delete("machine_printlog_count_cache")
